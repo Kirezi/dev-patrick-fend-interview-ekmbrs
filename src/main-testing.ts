c0e0d2fbb5726fa524d0c7e01a2c310acecb5643
Kirezi <kizpatrick.kp@gmail.com>
2021-10-27 19:44:01 -0400

    1 file modified

--- src/main-testing.ts
+++ src/main-testing.ts
@@ -1,29 +1,29 @@
-import "./global-jasmine";
-import "jasmine-core/lib/jasmine-core/jasmine-html.js";
-import "jasmine-core/lib/jasmine-core/boot.js";
+import './global-jasmine';
+import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
+import 'jasmine-core/lib/jasmine-core/boot.js';
 
-import "./polyfills";
+import './polyfills';
 
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-import "zone.js/dist/async-test";
-import "zone.js/dist/fake-async-test";
-import "zone.js/dist/long-stack-trace-zone";
-import "zone.js/dist/proxy.js";
-import "zone.js/dist/sync-test";
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
 
 // Requires 'zone.js/dist/proxy.js' and 'zone.js/dist/sync-test';
-import "zone.js/dist/jasmine-patch";
+import 'zone.js/dist/jasmine-patch';
 
-import { getTestBed } from "@angular/core/testing";
+import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from "@angular/platform-browser-dynamic/testing";
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
 
 // stuff to test
-import "./app/app.component.spec.ts";
-import "./app/user/user.component.spec.ts";
-import "./app/user/user.service.spec.ts";
+import './app/app.component.spec.ts';
+import './app/user/user.component.spec.ts';
+import './app/service/user.service.spec.ts';
 
 jasmine.getEnv().configure({ random: false });
 bootstrap();
